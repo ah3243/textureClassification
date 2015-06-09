cecdf9d3fd904ae66716692bd0e72b692f56a28b
Jamestt <9a61c270@opayq.com>
2015-06-10 00:50:27 +0100

    basic's of bw k-means, data type problems still

--- OpenCv Programs/4 TestFilters/kmeansExample/kmeansTest.cpp
+++ OpenCv Programs/4 TestFilters/kmeansExample/kmeansTest.cpp
@@ -1,32 +1,58 @@
+/*Resource "http://stackoverflow.com/questions/10240912/input-matrix-to-opencv-kmeans-clustering"*/
+#include <iostream>
+
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv ) {
-  Mat src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  Mat samples(src.rows * src.cols, 3, CV_32F);
+  // Read grayscale image in
+  Mat src = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  namedWindow("clustered image", WINDOW_AUTOSIZE);
+
+  // Confirm image found
+  if (!src.data) {
+    cout << "No input data found exiting" << endl;
+    return -1;
+  }
+
+  // Convert input image to a float
+  src.convertTo(src, CV_32F);
+
+  // Create sample Mat, same size as input image
+  Mat samples(src.rows * src.cols, 1, CV_32F);
+
+  // Copy across input image
   for (int y = 0; y < src.rows; y++)
     for (int x = 0; x < src.cols; x++)
-      for (int z = 0; z < 3; z++)
-        samples.at<float>(y + x*src.rows, z) = src.at<Vec3b>(y, x)[z];
+      samples.at<float>(y, x) = src.at<float>(y, x);
 
-
+  // KMeans parameters
   int clusterCount = 10;
   Mat labels;
   int attempts = 5;
   Mat centers;
+  // Apply KMeans
   kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
 
 
-  Mat new_image(src.size(), src.type() );
-  for (int y = 0; y < src.rows; y++ )
+   Mat new_image(src.size(), src.type() );
+  for (int y = 0; y < src.rows; y++) {
     for (int x = 0; x < src.cols; x++) {
       int cluster_idx = labels.at<int>(y + x*src.rows, 0);
-      new_image.at<Vec3b>(y, x)[0] = centers.at<float>(cluster_idx, 0);
-      new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
-      new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
+      new_image.at<float>(y, x) = centers.at<float>(cluster_idx);
+      // new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
+      // new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
     }
-  imshow("clustered image", new_image);
+  }
+
+  double minVal, maxVal;
+  minMaxLoc(src, &minVal, &maxVal);
+  Mat draw;
+  new_image.convertTo(draw, CV_8U, 255.0/(maxVal-minVal), -minVal * 255.0/(maxVal-minVal));
+
+  imshow("clustered image", draw);
   waitKey(0);
 }
