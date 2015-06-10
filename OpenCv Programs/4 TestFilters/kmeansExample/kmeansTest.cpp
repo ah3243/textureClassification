29bff488332b306d91ad57dd56ccb875e41de8b0
Jamestt <9a61c270@opayq.com>
2015-06-10 13:28:16 +0100

    remove output img, equalise histogram, print clusters

--- OpenCv Programs/4 TestFilters/kmeansExample/kmeansTest.cpp
+++ OpenCv Programs/4 TestFilters/kmeansExample/kmeansTest.cpp
@@ -1,4 +1,5 @@
 /*Resource "http://stackoverflow.com/questions/10240912/input-matrix-to-opencv-kmeans-clustering"*/
+#include <stdio.h>
 #include <iostream>
 
 #include "opencv2/imgproc/imgproc.hpp"
@@ -9,14 +10,18 @@ using namespace std;
 
 int main(int argc, char** argv ) {
   // Read grayscale image in
-  Mat src = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-  namedWindow("clustered image", WINDOW_AUTOSIZE);
+  Mat in, src;
+  in = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+//  namedWindow("clustered image", WINDOW_AUTOSIZE);
 
   // Confirm image found
-  if (!src.data) {
+  if (!in.data) {
     cout << "No input data found exiting" << endl;
     return -1;
   }
+
+  // Equalise input's histogram
+  equalizeHist(in, src);
 
   // Convert input image to a float
   src.convertTo(src, CV_32F);
@@ -38,21 +43,23 @@ int main(int argc, char** argv ) {
   kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
 
 
-   Mat new_image(src.size(), src.type() );
-  for (int y = 0; y < src.rows; y++) {
-    for (int x = 0; x < src.cols; x++) {
-      int cluster_idx = labels.at<int>(y + x*src.rows, 0);
-      new_image.at<float>(y, x) = centers.at<float>(cluster_idx);
-      // new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
-      // new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
-    }
-  }
+cout << "This is the cluster centers: " << endl << centers << endl;
 
-  double minVal, maxVal;
-  minMaxLoc(src, &minVal, &maxVal);
-  Mat draw;
-  new_image.convertTo(draw, CV_8U, 255.0/(maxVal-minVal), -minVal * 255.0/(maxVal-minVal));
-
-  imshow("clustered image", draw);
+  //  Mat new_image(src.size(), src.type() );
+  // for (int y = 0; y < src.rows; y++) {
+  //   for (int x = 0; x < src.cols; x++) {
+  //     int cluster_idx = labels.at<int>(y + x*src.rows, 0);
+  //     new_image.at<float>(y, x) = centers.at<float>(cluster_idx);
+  //     // new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
+  //     // new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
+  //   }
+  // }
+  //
+  // double minVal, maxVal;
+  // minMaxLoc(src, &minVal, &maxVal);
+  // Mat draw;
+  // new_image.convertTo(draw, CV_8U, 255.0/(maxVal-minVal), -minVal * 255.0/(maxVal-minVal));
+  //
+  // imshow("clustered image", draw);
   waitKey(0);
 }
