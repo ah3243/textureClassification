91114990ca50ecdf84ed6cf139563103f1363b4e
albertJ32 <00b0d14c@opayq.com>
2015-06-12 15:28:04 +0100

    import from dir, check extension, blend images

--- OpenCv Programs/4 TestFilters/kmeansExample/kmeansTest.cpp
+++ OpenCv Programs/4 TestFilters/kmeansExample/kmeansTest.cpp
@@ -1,6 +1,10 @@
-/*Resource "http://stackoverflow.com/questions/10240912/input-matrix-to-opencv-kmeans-clustering"*/
+
+#include <dirent.h>
+#include <opencv2/core/core.hpp>
+#include <string.h>
 #include <stdio.h>
 #include <iostream>
+#include <sstream>  // Used for imgpath string stream
 
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -8,58 +12,96 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv ) {
-  // Read grayscale image in
-  Mat in, src;
-  in = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-//  namedWindow("clustered image", WINDOW_AUTOSIZE);
 
-  // Confirm image found
-  if (!in.data) {
-    cout << "No input data found exiting" << endl;
-    return -1;
-  }
+// Check that file in dir is an accepted img type
+bool hasEnding (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
 
-  // Equalise input's histogram
-  equalizeHist(in, src);
-
-  // Convert input image to a float
-  src.convertTo(src, CV_32F);
-
-  // Create sample Mat, same size as input image
-  Mat samples(src.rows * src.cols, 1, CV_32F);
-
-  // Copy across input image
-  for (int y = 0; y < src.rows; y++)
-    for (int x = 0; x < src.cols; x++)
-      samples.at<float>(y, x) = src.at<float>(y, x);
-
+Mat kmeansFunc(Mat samples){
   // KMeans parameters
   int clusterCount = 10;
   Mat labels;
   int attempts = 5;
   Mat centers;
+
   // Apply KMeans
   kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
+  return centers;
+}
+
+void createSamples(Mat src) {
+  // Create sample Mat, same size as input image
+  Mat samples(src.rows * src.cols, 1, CV_32F);
+
+  // Copy across input image
+  for (int y = 0; y < src.rows; y++) {
+    for (int x = 0; x < src.cols; x++) {
+      samples.at<float>(y, x) = src.at<float>(y, x);
+    }
+  }
+
+  // Put combined Mat through kmeans
+  Mat centers2 = kmeansFunc(samples);
+  cout << "This is the cluster centers of two images: " << endl << centers2 << endl;
+  return;
+}
+
+int main(int argc, char** argv) {
+  namedWindow("testWindow", 1);
+
+/*-------------------------- Load All images from Folder ----------------------------*/
+  cv::Mat img, aggImg;
+
+  std::string extTypes[] = {".jpg", ".png", ".bmp"};
+  std::string dirNme = "../../TEST_IMAGES/";
+  DIR *dir;
+  dir = opendir(dirNme.c_str());
+  string imgName;
+  struct dirent *ent;
 
 
-cout << "This is the cluster centers: " << endl << centers << endl;
+  int cnt = 0;
+  if (dir != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      imgName = ent->d_name;
+      if (hasEnding(imgName, ".jpg")) {
+        // Sort out string Stream
+        std::stringstream ss;
+        ss << dirNme << imgName;
+        std::string imgpath = ss.str();
 
-  //  Mat new_image(src.size(), src.type() );
-  // for (int y = 0; y < src.rows; y++) {
-  //   for (int x = 0; x < src.cols; x++) {
-  //     int cluster_idx = labels.at<int>(y + x*src.rows, 0);
-  //     new_image.at<float>(y, x) = centers.at<float>(cluster_idx);
-  //     // new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
-  //     // new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
-  //   }
-  // }
-  //
-  // double minVal, maxVal;
-  // minMaxLoc(src, &minVal, &maxVal);
-  // Mat draw;
-  // new_image.convertTo(draw, CV_8U, 255.0/(maxVal-minVal), -minVal * 255.0/(maxVal-minVal));
-  //
-  // imshow("clustered image", draw);
-  waitKey(0);
+        // Read in image
+        Mat in = imread(imgpath, CV_LOAD_IMAGE_GRAYSCALE);
+         equalizeHist(in, img);
+         cout << "importing File: " << imgpath << endl;
+        if (cnt ==0) {
+          aggImg = img;
+        } else {
+          addWeighted(aggImg, 0.5, img, 0.5, 0.0, aggImg);
+        }
+
+        imshow("testWindow", aggImg);
+
+      } else {
+      cout << "wrong extension detected: " << imgName << endl;
+      }
+      cnt++;
+    }
+    closedir(dir);
+  } else {
+    cout << "dir not found" << endl;
+  }
+
+/*-------------------------- Cluster aggregated images ----------------------------*/
+  waitKey();
+  // Convert input image to a float
+  aggImg.convertTo(aggImg, CV_32F);
+  createSamples(aggImg);
+
+  return 0;
 }
