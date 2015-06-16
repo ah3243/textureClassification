120f13831163868f4d551be1c4a97bf16517330b
albertJ32 <00b0d14c@opayq.com>
2015-06-16 12:23:12 +0100

    replace gabor with gaussian x,y, remove superfluous code

--- OpenCv Programs/4 TestFilters/gaussian_Log_Filters/gaussian_log_gabor.cpp
+++ OpenCv Programs/4 TestFilters/gaussian_Log_Filters/gaussian_log_gabor.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
 /*Copyright 2015*/
+#include <stdio.h>
 
 #include <stdlib.h>
 #include <sys/types.h>  // used for dir creation
@@ -20,26 +20,22 @@ using namespace std;
 
 /*Gabor filter globals*/
 int kernelSize = 21;
-double sigma = 10, theta = 1, lambd = 20, gm = 0, psi = 0;
-cv::Mat dst, dst1, dst_gab, src, src_gab, input, in, output, kernel, kernel_gab;
+int imgCount = 0;
+cv::Mat src, dst, dst1, dst2, input;
 
 // Gabor filter function
-void process(double angle, void *) {
-  theta = angle;
-  kernel_gab = cv::getGaborKernel(cv::Size(kernelSize, kernelSize), sigma, theta, lambd, gm, psi);
-  cv::filter2D(src_gab, dst_gab, CV_32F, kernel_gab);
+void process(int x, int y) {
+  GaussianBlur(src, dst2, Size(), x, y, BORDER_DEFAULT);
 }
 
+int saveImg(Mat img, string imgType);
 
 int main(int argc, char** argv) {
-
   // Create variables
-  Point anchor;
-  double delta;
   int ddepth = CV_16S;
-  const char *window_name1 = "Gaussian";
-  const char *window_name2 = "Laplacian of Gaussian";
-  const char *window_name3 = "GaborFilters";
+  string window_name1 = "Gaussian";
+  string window_name2 = "Laplacian of Gaussian";
+  string window_name3 = "Anisometric Filters";
 
   // Load image in grayscale
   input = imread(argv[1], CV_LOAD_IMAGE_COLOR);
@@ -50,97 +46,101 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  cvtColor(input, input, CV_BGR2GRAY);  // convert color img to grayscale
+  // convert color img to grayscale
+  cvtColor(input, input, CV_BGR2GRAY);
 
   // Create a named window
   namedWindow(window_name1, WINDOW_AUTOSIZE);
 
-  /* Shows original image*/
+/* ------------------------ Show original image ------------------------ */
   const char *window_name = "Source image";
   imshow(window_name, input);
 
   waitKey();
 
-  equalizeHist(input, src);  // Equalise the input image, save in src
+/* ---------------- Equalise Histogram and show image ------------------ */
+  // Equalise the input image, save in src
+  equalizeHist(input, src);
 
+  // Show equalized image
   imshow(window_name, src);
   waitKey();
 
-  // // Initialise filter arguments
-  // anchor = Point(-1, -1);
-  // delta = 0;
-  // ddepth = -1;
-  //
-  // kernel = (Mat_<char>(3, 3) << 1, 1, 1,
-  //                              1,  1, 1,
-  //                               1, 1,  1);
-  //
-  // // Run the Filter
-  // filter2D(src, dst, ddepth, kernel, anchor, delta, BORDER_DEFAULT);
-
+/* ------------------------ Gaussian Filter ----------------------------- */
   // Blur original image and display
-  GaussianBlur(src, dst, Size(11, 11), 1, 0, BORDER_DEFAULT);
+  GaussianBlur(src, dst, Size(11, 11), 0, 0, BORDER_DEFAULT);
+
+  // Save image
+  saveImg(dst, "Gaussian_filter");
 
   // Show dst Mat in created window
-   imshow(window_name1, dst);
+  imshow(window_name1, dst);
 
+
+/* ------------------------ Laplician of Gaussian -------------------------- */
+  //LoG output hold Mat
   Mat SclConv;
-  // Apply Laplacian to Blured image and display
 
-  Laplacian(dst, dst1, ddepth, 11, 1, 0, BORDER_DEFAULT);
+  // Apply Laplacian to Blured image and display
+  Laplacian(src, dst1, ddepth, 11, 1, 0, BORDER_DEFAULT);
 
   // Convert output from Laplacian operator to CV_8U image
   convertScaleAbs(dst1, SclConv);
 
-
   // Show dst Mat in created window
   imshow(window_name2, dst1);
 
+  // Save image
+  imgCount = 0;
+  saveImg(dst1, "LoG_filter");
+
   // Wait for anykey before exit
-  waitKey(0);
+  waitKey();
+
+/* ------------------------ Bar and Edge filters -------------------------- */
+  double filterAngle[6] = {1, 3, 2, 6, 4, 12};
+
+  // time_t t = time(0);   // get time now
+  // // Print out current time and date
+  //     struct tm * now = localtime(& t);
+  //     cout << (now->tm_year + 1900) << '-'
+  //          << (now->tm_mon + 1) << '-'
+  //          <<  now->tm_mday
+  //          << endl;
 
 
+  imgCount = 0;
+  for (int i = 1; i < 7; i+=2) {
+    process(filterAngle[i], filterAngle[i+1]);
+
+    // Save image
+    saveImg(dst2, "bar_Fitler");
+
+    // Display Kernel
+    imshow("Gabor Filter", dst2);
+    waitKey(0);
+  }
+
+  return 0;
+}
+
+int saveImg(Mat img, string imgType) {
   string imgpath = "../savImgs/test";
+
   string fullpath = "mkdir " + imgpath;
   system(fullpath.c_str());   // create dir for filtered images
 
-std::stringstream ss;
+  std::stringstream ss;
 
-std::string imgname = "/example_name";
-std::string type = ".png";
+  std::string imgname = "/" + imgType;
+  std::string type = ".png";
 
-double filterAngle[6] = {0, 60, 120, 180, 240, 300};
-
-// time_t t = time(0);   // get time now
-// // Print out current time and date
-//     struct tm * now = localtime(& t);
-//     cout << (now->tm_year + 1900) << '-'
-//          << (now->tm_mon + 1) << '-'
-//          <<  now->tm_mday
-//          << endl;
-
-
-/*Gabor Filters*/
-src.convertTo(src_gab, CV_32F);
-
-for (int i = 1; i < 7; i++) {
-  ss << imgpath << imgname << i << type;
+  ss << imgpath << imgname << imgCount << type;
   std::string fileName = ss.str();
   ss.str("");
 
-  // Print out theta conv to int
+  // Print out filename
   cout << "This is the filename " << fileName << endl;
-
-  process(filterAngle[i], 0);
-  dst_gab.convertTo(output, CV_8U, 1.0/255.0);
-
-  imwrite(fileName, output);
-
-  // Display Kernel
-  imshow(window_name3, kernel_gab);
-  imshow("Gabor Filter", output);
-  waitKey(0);
-}
-
-  return 0;
+  imwrite(fileName, img);
+  imgCount++;
 }
